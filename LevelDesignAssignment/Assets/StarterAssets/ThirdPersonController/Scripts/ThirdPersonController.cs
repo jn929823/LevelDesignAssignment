423497375150b695636b024fc3e914b2b4410a5e
jn929823 <jn929823@ohio.edu>
2025-03-24 20:38:18 -0400

    added terrarin and playerSensitivity

--- LevelDesignAssignment/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
+++ LevelDesignAssignment/Assets/StarterAssets/ThirdPersonController/Scripts/ThirdPersonController.cs
@@ -75,6 +75,9 @@ namespace StarterAssets
         [Tooltip("For locking the camera position on all axis")]
         public bool LockCameraPosition = false;
 
+        // sensitivity
+        public Vector2 playerSensitivity;
+
         // cinemachine
         private float _cinemachineTargetYaw;
         private float _cinemachineTargetPitch;
@@ -198,8 +201,8 @@ namespace StarterAssets
                 //Don't multiply mouse input by Time.deltaTime;
                 float deltaTimeMultiplier = IsCurrentDeviceMouse ? 1.0f : Time.deltaTime;
 
-                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier;
-                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier;
+                _cinemachineTargetYaw += _input.look.x * deltaTimeMultiplier * playerSensitivity.x;
+                _cinemachineTargetPitch += _input.look.y * deltaTimeMultiplier * playerSensitivity.y;
             }
 
             // clamp our rotations so our values are limited 360 degrees
